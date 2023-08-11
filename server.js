441c6e7986a753c99e475d9cf1b1275460a01473
Yogesh Gupta <yogesh.gupta38@gmail.com>
2023-08-11 13:26:58 +0530

    added login route and password hashing

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const db = require("./app/config/db.config");
+const bcrypt = require('bcryptjs');
 
 const app = express()
 
@@ -15,12 +18,66 @@ app.use(express.json());
 // parse requests of content type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended:true}))
 
+
 app.get("/",(req,res)=>{
     res.json({message:"Welcome to  my application"})
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT,()=>{
     console.log('server is running on port',PORT);
+})
+
+db.connect(err=>{
+    if(err){
+        console.log('error creating connection to db')
+    }else{
+        console.log('connted to mysqlDB')
+    }
+})
+
+// create user
+
+app.post("/api/signup",async(req,res)=>{
+    let userName = req.query.user_name
+    let userEmail = req.query.user_email
+    let userPassword = bcrypt.hashSync(req.query.user_pass,10)
+    try {
+        db.query('INSERT INTO users(userName,userEmail,userPassword) VALUES (?,?,?)',[userName,userEmail,userPassword],(err,result)=>{
+            if(err){
+                res.status(404).send({message:'Could not create the user, try again'})
+            }else{
+                res.status(200).send(result)
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// login user
+
+app.post("/api/signin",(req,res)=>{
+    const user_pass = req.query.user_pass
+    console.log(req.query.user_name,req.query.user_pass)
+    try {
+        db.query(`SELECT * FROM users WHERE userName = '${req.query.user_name}'`,(err,result)=>{
+            if(err){
+                res.status(404).send({message:'No such user found'})
+            }else{
+                const hashedPass = result[0].userPassword
+                const passwordMatches = bcrypt.compareSync(user_pass, hashedPass);
+                if (passwordMatches) {
+                    // generate jwt token here and send in response
+                    res.status(200).send({message:"password matches"});
+                } else {
+                    res.status(401).send({ message: 'Incorrect password' });
+                }
+            }
+        })
+    } catch (error) {
+        
+    }
 })
